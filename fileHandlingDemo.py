49d955b982e2500f75f150b206eeac22f539af5a
Durga2408 <144308242+Durga2408@users.noreply.github.com>
2023-09-20 12:30:01 +0530

    Add files via upload

---
+++ fileHandlingDemo.py
@@ -0,0 +1,28 @@
+import os
+
+# os.mkdir("d:\Kamal")
+#kamalfp=open("d:\\Kamal\\sisvafile.txt","w+")
+#kamalfp.write("kiran is absent")
+#print("the file is completed")
+#kamalfp=open("d:\\Kamal\\sisvafile.txt1","w")
+#kamalfp.write("kiran is absent")
+#print("the file is completed")
+#kamalfp=open("d:\\Kamal\\sisvafile.txt2","r")
+#kamalfp.write("kiran is absent")
+#print("the file is completed")
+kamalfp=open("d:\\Kamal\\sisvafile.txt3","r+")
+kamalfp.write("kiran is absent")
+print("the file is completed")
+#kamalfp=open("d:\\Kamal\\sisvafile.txt4","a+")
+#kamalfp.write("kiran is absent")
+#print("the file is completed")
+#kamalfp=open("d:\\Kamal\\sisvafile.txt5","a")
+#kamalfp.write("kiran is absent")
+#print("the file is completed")
+kamalfp=open("d:\\Kamal\\sisvafile.txt6","x")
+kamalfp.write("kiran is absent")
+print("the file is completed")
+
+
+
+
